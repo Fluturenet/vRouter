4b61afbbb4ca2bf6ee75f829f34e66badafa6ea6
fluturenet <fluturenet@github.com>
2017-05-28 12:20:33 +0200

    Working version with example

---
+++ example/example.js
@@ -0,0 +1,13 @@
+var router = require("../index.js")
+var node1 = router.newNode()
+var node2 = router.newNode()
+
+var socket1 = node1.createSocket('udp4')
+socket1.bind(1234)
+socket1.on("message",function(msg,rinfo){console.log(">",msg)})
+
+var socket2 = node2.createSocket('udp4')
+socket2.on("message",function(msg,rinfo){console.log("Talking to myself")})
+
+socket2.send("IT WORKS!!",1234,"10.0.0.1")
+socket2.send("IT WORKS!!",socket2.port,"10.0.0.2")
