cec3ec2e6cb2f812b09dcd275cddf031527dda54
fluturenet <fluturenet@github.com>
2017-05-31 07:48:05 +0200

    implemente address() function and added bind cb

--- example/example.js
+++ example/example.js
@@ -7,7 +7,7 @@ socket1.bind(1234)
 socket1.on("message",function(msg,rinfo){console.log(">",msg)})
 
 var socket2 = node2.createSocket('udp4')
-socket2.on("message",function(msg,rinfo){console.log("Talking to myself")})
+socket2.on("message",function(msg,rinfo){console.log("Talking to myself",rinfo.address,rinfo.port)})
 
 socket2.send("IT WORKS!!",1234,"10.0.0.1")
 socket2.send("IT WORKS!!",socket2.port,"10.0.0.2")
